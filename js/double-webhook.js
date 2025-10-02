f39f080e6da743377c7a0b7b45afebfb4b333499
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2025-10-02 21:48:16 +0200

    Fix error in double-webhook.js

--- js/double-webhook.js
+++ js/double-webhook.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    n8nWebhookUrl: 'https://n8n.j-aime.fr/webhook/mnidesk-lead-process_v3', // ← Ici
+    n8nWebhookUrl: 'https://n8n.j-aime.fr/webhook/omnidesk-lead-process_v3', // ← Ici
     formId: 'sib-form',
     debug: true, // Mettre à false en production
     retryAttempts: 2,
